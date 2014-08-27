07243c0f3b2e57cc1ed1c30bab24c4123d01a38c
Eduardo Acosta <eacosta12@gmail.com>
2014-08-27 18:30:04 -0500

    Updated with Slick Carousel

---
+++ floor/js/slick-scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.slick').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        slide: '> div'
+        
+    });
+
+
+    
+});    
+
